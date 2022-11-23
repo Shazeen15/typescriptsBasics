5949b54e242b87b69af017c69980045a8790aa2b
Shazeen Fabius <shazeen.fabius@measurabl.com>
2022-11-22 16:20:13 -0800

    first commit

---
+++ example.ts
@@ -0,0 +1,6 @@
+function addShipping(price: number, shipping: number) : number {
+    console.log(price + shipping);
+    return price + shipping;
+}
+
+addShipping(10, 5);
---
+++ variables.ts
@@ -0,0 +1,6 @@
+// primitive data type: string, number, boolean, null, underfined
+let myName: string = 'Shazeen';
+myName = 'jamile'
+
+let myName2 = '';
+myName2 = 'leslie';
